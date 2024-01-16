a2397a885eaa106a95c558f2387b4f0539bee28c
Jerpharney <jerpharney@gmail.com>
2024-01-16 04:24:17 +0100

    character movement and animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,17 +1,16 @@
 extends CharacterBody2D
 
-
 @export var speed : float = 200.0
-@export var jumpVelocity : float = -200.0
+@export var jumpVelocity : float = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		$PlayerAnimation.play("Jump")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -22,7 +21,21 @@ func _physics_process(delta):
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * speed
+		$PlayerAnimation.play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+		if is_on_floor():
+			$PlayerAnimation.play("Idle")
 
+	update_facing_direction()
 	move_and_slide()
+
+func update_facing_direction():
+	if Input.get_action_strength("left") and Input.get_action_strength("right"):
+		return
+	if Input.get_action_strength("left") and $Sprite.flip_h != true:
+		$Sprite.flip_h = true
+		$PlayerAnimation.play("Turn Around")
+	elif Input.get_action_strength("right") and $Sprite.flip_h != false:
+		$Sprite.flip_h = false
+		$PlayerAnimation.play("Turn Around")
